983cd4a0624f632d32a7d6fc072a7b7a77414cc1
owjoel <joel.ow.2022@smu.edu.sg>
2024-11-10 21:13:59 +0800

    fix(request): update otel

--- apps/request/tracing.js
+++ apps/request/tracing.js
@@ -7,20 +7,19 @@ const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grp
 const { Resource } = require('@opentelemetry/resources');
 const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
 const { RuntimeNodeInstrumentation } = require("@opentelemetry/instrumentation-runtime-node");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 
-console.log("Tracing started for ORDERS");
+console.log("Tracing started for REQUEST");
 
 const resource = new Resource({
-  'service.name': 'orders',
   'environment': process.env.OTEL_ENVIRONMENT
 });
 
 const traceExporter = new OTLPTraceExporter({ url: process.env.OTEL_HOST_URL });
 const metricExporter = new OTLPMetricExporter({ url: process.env.OTEL_HOST_URL });
-
 const metricReader = new PeriodicExportingMetricReader({
   exporter: metricExporter,
-  interval: 30000
+  exportIntervalMillis: 30000
 })
 
 const sdk = new opentelemetry.NodeSDK({
@@ -31,6 +30,7 @@ const sdk = new opentelemetry.NodeSDK({
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
     new RuntimeNodeInstrumentation({
       monitoringPrecision: 5000,
     }),
@@ -47,7 +47,7 @@ const sdk = new opentelemetry.NodeSDK({
       },
     }),
   ],
-  serviceName: process.env.SERVICE,
+  serviceName: 'request',
 });
 
 sdk.start();

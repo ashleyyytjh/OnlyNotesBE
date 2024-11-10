d0bbd7cdbeeac7ee2e4caf94dd629a9a1db0d5f5
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-11-10 15:49:45 +0800

    chore: Clean up unused imports

--- apps/notes/src/main/java/cs302/notes/security/JwtAuthenticationFilter.java
+++ apps/notes/src/main/java/cs302/notes/security/JwtAuthenticationFilter.java
@@ -1,29 +1,22 @@
 package cs302.notes.security;
 
-import cs302.notes.exceptions.UnauthorizedException;
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;

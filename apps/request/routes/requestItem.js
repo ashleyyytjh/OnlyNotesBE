40160d5edf905b0b940d1a6fafa8ddcaf2e59c8c
EddyEhhh <eyyeddy@gmail.com>
2024-11-10 22:35:47 +0800

    update(request): secure request endpoints

--- apps/request/routes/requestItem.js
+++ apps/request/routes/requestItem.js
@@ -4,9 +4,9 @@ const requestItemController = require('../controllers/requestItemController');
 const {tokenValid} = require('token-verifier-mee-rebus');
 
 router.get('/:requestId',tokenValid, requestItemController.getRequestItemById);
-router.get('/', requestItemController.getAllRequestItem);
-router.post('/', requestItemController.createRequest);
-router.put('/:requestId', requestItemController.updateRequestById);
-router.delete('/:requestId', requestItemController.deleteRequestById);
+router.get('/',tokenValid, requestItemController.getAllRequestItem);
+router.post('/',tokenValid, requestItemController.createRequest);
+router.put('/:requestId',tokenValid, requestItemController.updateRequestById);
+router.delete('/:requestId',tokenValid, requestItemController.deleteRequestById);
 
 module.exports = router;
